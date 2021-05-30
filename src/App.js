48fbfdda726e306bd5df79e48fe935037b6c6e0e
unknown <68383156+orynek@users.noreply.github.com>
2021-05-30 23:46:18 +0300

    first version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,163 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Typography, Grid, Container, Fab, LinearProgress } from '@material-ui/core';
+import Timebar from './components/Timebar';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import StopIcon from '@material-ui/icons/Stop';
+import PauseIcon from '@material-ui/icons/Pause';
+import { makeStyles, ThemeProvider, createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
+let interv;
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(2),
+  },
+  wraper: {
+    marginTop: '5vh',
+    flexDirection: "column",
+    fontSize: "3rem",
+    "@media (min-width:600px)": {
+      fontSize: "3.5rem",
+    },
+  },
+  head: {
+    margin: "5vh",
+    fontFamily: "Barlow, sans-serif",
+  },
+  buttons: {
+    display: "flex",
+    justifyContent: "center",
+    ailgnItems: "center",
+    marginBottom: '7vh',
+    marginTop: "auto",
+  },
+  siki: {
+    display: "inline-block",
+    width: "2ch",
+  }
+}));
 
 function App() {
+  let seconds;
+  let minutes;
+  let hours;
+  const signalSound = document.getElementById('signalSound');
+  const alarmSound = document.getElementById('alarmSound');
+  const classes = useStyles();
+  const [isTicking, setIsTicking] = useState(false);
+  const [signalMinutes, setSignalMinutes] = useState(20 * 60);
+  const [alarmMinutes, setAlarmMinutes] = useState(10 * 60);
+  const [allSeconds, setAllSeconds] = useState(signalMinutes + alarmMinutes);
+  const [resetStatus, setResetStatus] = useState(true);
+  
+  seconds = allSeconds % 60;
+  minutes = Math.floor((allSeconds / 60) % 60);
+  hours = Math.floor((allSeconds / 60) / 60);
+  seconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+  minutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  hours = hours < 10 ? `0${hours}` : `${hours}`;
+  
+  useEffect(() => {
+    if (allSeconds === signalMinutes) {
+      alarmSound.play();
+    }
+    if (!allSeconds) {
+      signalSound.play();
+      setIsTicking((prev) => !prev);
+    }
+  }, [allSeconds]);
+  
+  useEffect(() => {
+    if (isTicking) {
+      interv = setInterval(() => {
+        setAllSeconds((prev) => (prev ? prev - 1 : prev));
+      }, 1000);
+      return () => clearInterval(interv);
+    }
+    return undefined;
+  }, [isTicking]);
+
+  useEffect(() => {
+    setAllSeconds(signalMinutes + alarmMinutes);
+  }, [signalMinutes, alarmMinutes]);
+
+  const handleChange = (time, section) => {
+    section === "Signal"
+      ? setSignalMinutes(time * 60)
+      : setAlarmMinutes(time * 60);
+  };
+
+  const handleReset = () => {
+    setIsTicking(false);
+    setSignalMinutes(20 * 60);
+    setAlarmMinutes(10 * 60);
+    setAllSeconds(signalMinutes + alarmMinutes);
+    setResetStatus((prev) => !prev);
+    signalSound.load();
+  };
+  let MAX = 0;
+  let MIN = signalMinutes + alarmMinutes;
+  const normalise = value => (value - MIN) * 100 / (MAX - MIN)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="sm" style={{display: 'flex', flexDirection: 'column', height: '100vh'}}>
+        {/* <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly'}}> */}
+        
+        <LinearProgress variant="determinate" value={normalise(allSeconds)} />
+        <Typography className={classes.head} align="center" variant="h1">
+          <div className={classes.siki}>{hours}</div>
+          :
+          <div className={classes.siki}>{minutes}</div>
+          :
+          <div className={classes.siki}>{seconds}</div>
+        </Typography>
+        <Grid container className={classes.wraper}>
+          <Timebar
+            onTimeChange={handleChange}
+            resetChange={resetStatus}
+            section="Signal"
+            isTicking={isTicking}
+          />
+          <Timebar
+            onTimeChange={handleChange}
+            resetChange={resetStatus}
+            section="Alarm"
+            isTicking={isTicking}
+          />
+        </Grid>
+        <div className={classes.buttons}>
+          {isTicking ? (
+            <Fab
+              color="secondary"
+              className={classes.margin}
+              onClick={() => setIsTicking(false)}
+            >
+              <PauseIcon />
+            </Fab>
+          ) : (
+            <Fab
+              color="primary"
+              className={classes.margin}
+              onClick={() => setIsTicking(true)}
+            >
+              <PlayArrowIcon />
+            </Fab>
+          )}
+          <Fab
+            color="secondary"
+            className={classes.margin}
+            onClick={() => handleReset()}
+          >
+            <StopIcon />
+          </Fab>
+          
+        </div>
+        {/* </div> */}
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Timebar.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles, createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
+import { Typography, Grid, FormControl, Select, MenuItem } from '@material-ui/core';
+import VolumeUpIcon from '@material-ui/icons/VolumeUp';
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
+
+const fnt = 'Barlow, sans-serif';
+
+const useStyles = makeStyles((theme) => ({
+  papier: {
+    justify: "center",
+    margin: "15px 5px 0px",
+  },
+  picker: {
+    marginLeft: "50%",
+  },
+  tytul: {
+    textTransform: "uppercase",
+    marginLeft: "20%",
+    fontFamily: fnt,
+  },
+  numb: {
+    fontFamily: fnt,
+    fontSize: "2.5rem",
+    "@media (min-width:600px)": {
+      fontSize: "3rem",
+    },
+  }
+}));
+
+export default function Timebar({onTimeChange, section, isTicking, resetChange}) {
+    const classes = useStyles();
+    const [pickedTime, setPickedTime] = useState(section==="Signal"? 20 : 10)
+    const passTime = (e) => {
+      onTimeChange(e.target.value, section);
+      setPickedTime(e.target.value);
+    }
+    useEffect(()=>{
+      setPickedTime(section==="Signal"? 20 : 10);
+    },[resetChange])
+
+    return (
+      <ThemeProvider theme={theme}>
+      <Grid item className={classes.papier}>
+        <Typography className={classes.tytul} variant="h4"><VolumeUpIcon fontSize='small'/>{section}</Typography>
+        <FormControl className={classes.picker} >
+          <Select
+            className={classes.numb}
+            disabled={isTicking}
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            type="number"
+            value={pickedTime}
+            defaultValue={section==="Signal"? 20 : 10}
+            onChange={passTime}
+          >
+            <MenuItem value={0.05}>0.05</MenuItem>
+            <MenuItem value={10}>10</MenuItem>
+            <MenuItem value={15}>15</MenuItem>
+            <MenuItem value={20}>20</MenuItem>
+            <MenuItem value={25}>25</MenuItem>
+            <MenuItem value={30}>30</MenuItem>
+            <MenuItem value={35}>35</MenuItem>
+            <MenuItem value={40}>40</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+      </ThemeProvider>
+    );
+}
